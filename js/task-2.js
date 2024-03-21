6de1cf0ff8525fa2ff0fd289f31442b8c5faf1de
ArtemWQW <149912826+ArtemWQW@users.noreply.github.com>
2024-03-22 01:07:42 +0200

    hw-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,8 @@
 function formatMessage(message, maxLength) {
 
-    if(message <= maxLength) {
-        return message} else {
-            return message.slice(0,maxLength) + "...";
+    if (message.length > maxLength) {
+        return message.slice(0,maxLength) + "...";} else {
+            return message
         } 
 }
 
--- js/task-4.js
+++ js/task-4.js
@@ -1,15 +1,15 @@
 function getShippingCost(country) {
 
     switch (country) {
-        case "Australia" : `Shipping to ${country} will cost 170 credits`;
+        case "Australia": return `Shipping to ${country} will cost 170 credits`;
         break;
-        case "China" : `Shipping to ${country} will cost  credits`;
+        case "China": return `Shipping to ${country} will cost 100 credits`;
         break;
-        case "Chile" : `Shipping to ${country} will cost  credits`;
+        case "Chile": return `Shipping to ${country} will cost 250 credits`;
         break;
-        case "Jamaica" : `Shipping to ${country} will cost  credits`;
+        case "Jamaica": return `Shipping to ${country} will cost 120 credits`;
         break; 
-        
+        default: return "Sorry, there is no delivery to your country";
     }
 }
 
